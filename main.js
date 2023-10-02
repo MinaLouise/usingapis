58d6715f940f554cfd6fda9284925b85e6cef33d
Mina <msanford24@basecampcodingacademy.org>
2023-10-02 16:17:01 -0500

    can't print

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-// 
+// Define the URL of the API
+// const apiUrl = "https://dictionaryapi.com/api/v3/references/spanish/json/test?key=86cfc412-5fa0-4101-a013-9d82512e970d";
+
+// fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data)=>{
+//         const button = document.getElementsByClassName("enter")
+
+//         button.addEventListener("click", function() {
+//             document.querySelector("spanish").innerHTML = "BCCA"
+           
+//         })
+
+//     })
+//     .catch(error =>{
+//         console.log("Error")
+//     })
+
+
+// button.addEventListener("click", () =>{
+//     document.getElementsByClassName("spanish")
+   
+// });
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function() {
+   const spanish = document.getElementsByClassName('spanish')
+   console.log('BCCA');
+
+   
+})
+   
